af555b71e0e724bab4fc199dbc1a30f2ff5bd33b
Michael Garcia <84154629+michael-alpinedata@users.noreply.github.com>
2026-06-09 16:01:41 +0200

    Update api.py to enhance logs visibility on streamlit dashboard
    reset uvicorn log config to use our config)

--- src/wetall_scanner/api.py
+++ src/wetall_scanner/api.py
@@ -120,4 +120,4 @@ if __name__ == "__main__":
         import uvicorn
 
         port = int(os.environ.get("PORT", 10000))
-        uvicorn.run(app, host="0.0.0.0", port=port)
+        uvicorn.run(app, host="0.0.0.0", port=port, log_config=None)
